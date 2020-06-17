a815c619a6a942612bdb949f22537f8c474b3382
Hubert MUGABO <mugberto@gmail.com>
2020-06-17 15:04:29 +0200

    Refactor my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,14 +1,11 @@
 module Enumerable
   def my_each
-    collection = self
-    if collection.is_a? Hash
-      for element in collection
-        yield element[0], element[1]
-      end
-    else
-      for element in collection
-        yield element
-      end
+    for element in self
+      element.is_a?(Array) ? yield(element[0], element[1]) : yield(element)
     end
   end
 end
+
+# { liquid: 'water', solid: 'rock' }.my_each {|k, v| p "#{k} and #{v}"}
+# [5,5,8,2,3].my_each {|i| p i}
+# (1...10).my_each {|i| p i}
