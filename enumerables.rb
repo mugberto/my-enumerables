3cec1b8e007df9af411a2a1e564a2d8de720afdf
Hubert MUGABO <mugberto@gmail.com>
2020-06-17 14:18:44 +0200

    Add my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,14 @@
 module Enumerable
-  
+  def my_each
+    collection = self
+    if collection.is_a? Hash
+      for element in collection
+        yield element[0], element[1]
+      end
+    else
+      for element in collection
+        yield element
+      end
+    end
+  end
 end
