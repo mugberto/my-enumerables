7f229383ca49a59dbca819ff5a8cf1825dadf5b0
Hubert MUGABO <mugberto@gmail.com>
2020-06-23 10:38:14 +0200

    Fix issues in #my_any? and #my_all?

--- enumerables.rb
+++ enumerables.rb
@@ -47,28 +47,30 @@ module Enumerable
     end
   end
 
-  def my_all?
-    return true unless block_given?
-
-    bool = true
-    case self
-    when Hash
-      my_each { |k, v| bool = false if yield(k, v) == false }
+  def my_all?(obj = Object)
+    arr = to_a
+    out_value = true
+    if block_given?
+      arr.my_each { |i| out_value = false unless yield(i) }
+    elsif obj.is_a?(Class)
+      arr.my_each { |i| out_value = false unless i }
     else
-      my_each { |i| bool = false if yield(i) == false }
+      arr.my_each { |i| out_value = false unless i == obj }
     end
-    bool
+    out_value
+  end
+
+  def my_any?(obj = Object)
+    arr = to_a
+    out_value = false
+    if block_given?
+      arr.my_each { |i| out_value = true if yield(i) }
+    elsif obj.is_a? Class
+      arr.my_each { |i| out_value = true if i.is_a?(obj) }
+    else
+      arr.my_each { |i| out_value = true if i == obj }
+    end
+    out_value
   end
 end
 
-# p({ liquid: 'water', solid: 'rock', gas: 'air', metal: 'copper' }.my_select { |_k, v| v.include? 'a' })
-# p (1..10).my_select
-# (1...10).my_each {|i| p i}
-# p [2,3,4,5].my_all? {|i| i.is_a? Numeric}
-# p(("a".."z").my_all? {|i| i.is_a? String} )
-# [["a", "b"], ["c", "d"]].my_each { |i| p i }
-# [2,3,4,5].my_each_with_index {|x| print x, " -- " }
-
-# hash = {}
-# %w[cat dog wombat].my_each_with_index
-#=> {"cat"=>0, "dog"=>1, "wombat"=>2}
