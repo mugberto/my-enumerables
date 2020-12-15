969d57ac87f08dd5b32136054b0823255ed2bf4f
menyagah <menyagah27@gmail.com>
2020-12-15 14:45:00 +0300

    Create Enumerable test class
     - Add my_each tests for:
       * Block not given
       * A hash
       * An array

---
+++ spec/my-enumerables_spec.rb
@@ -0,0 +1,19 @@
+require './enumerables'
+
+describe Enumerable do
+    describe '#my_each' do
+        it "should return an enumerator object if block is not given" do
+            obj = [1,2,4,5]
+            expect(obj.my_each.class).to eql(Enumerator)
+        end
+        it "should return number of  key and a value pair in a hash if block is given" do
+            obj = {man: 'Martin', age: 47}
+            expect(obj.my_each {|k,v| p "#{k}, #{v}"}).to eql(2)
+        end
+        it "should return number of  array items in an array if block is given" do
+            obj = [1,2,4,5]
+            expect(obj.my_each {|n| p "n is : #{n}"}).to eql(4)
+        end
+    end
+
+end
